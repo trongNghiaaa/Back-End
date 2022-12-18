6ef78a0d9bb7c2295d8ba6b9694ab746cfaedf4f
Nghia <nghiaa0801@gmail.com>
2022-12-18 20:51:00 +0700

    Buổi 6 : MySQL

--- Buoi2/buoi2.js
+++ Buoi2/buoi2.js
@@ -13,23 +13,21 @@ const warning = chalk.hex('#FFA500'); // Orange color
 
 console.log(error('Error!'));
 console.log(warning('Warning!'));
-// const server = http
-//     .createServer((request, response) => {
-//         fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
-//             if (data) {
-//                 response.writeHead(200, { 'Content-type': 'text' }, 'utf-8');
-//                 response.write(data, 'utf-8');
-//                 response.end();
-//             }
-//         });
-//     })
-//     .listen(3000, () => {
-//         console.log('Server đã khởi tạo thành công');
-//         // setTimeout(() => {
-//         //     exec('start chrome "http://localhost:3000/"');
-//         // }, 1000);
-//         spawn('C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe ', ['-new-tab', 'http://localhost:3000/']);
-//     });
+const server = http.createServer((request, response) => {
+        fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
+            if (data) {
+                response.writeHead(200, { 'Content-type': 'text' }, 'utf-8');
+                response.write(data, 'utf-8');
+                response.end();
+            }
+        });
+    }).listen(3000, () => {
+        console.log('Server đã khởi tạo thành công');
+        setTimeout(() => {
+            exec('start chrome "http://localhost:3000/"');
+        }, 1000);
+        //  spawn('C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe ', ['-new-tab', 'http://localhost:3000/']);
+    });
 
 // var url_to_open = 'http://http://localhost:3000/';
 // spawn('C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe ' , ['-new-tab', 'http://http://localhost:3000/']);
